68c329c441ddd23c03e13e4a9a19ef08a0f667a3
mine6cortes12-eng <mine6cortes12@gmail.com>
2026-05-07 10:55:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector("styles.css");
+const header = document.querySelector("mi-encabezado");
+const headerHeight = header.offsetHeight;
+window.addEventListener("scroll", () => {
+    if (window.scrollY > headerHeight) {
+        menu.style.backgroundColor = "rgb(50,43,150)";
+        } 
+        else{
+            menu.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        }
+    });
